6c4ebd5040ffa326bd46b4ec55649863a453b03a
anmitsux <abcdabcdec1@gmail.com>
2017-08-19 22:06:02 +0900

    rubyver

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  get "postss/like/:id" => "posts#like"
+  
+  get "posts/like/:id" => "posts#like"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
